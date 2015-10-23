81476fbd70326744183fb28f42b04411f45d3b42
Brad Lucas <brad@beaconhill.com>
2015-10-22 21:44:53 -0400

    manaully created solution

--- fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
+++ fox-goose-bag-of-corn/src/fox_goose_bag_of_corn/puzzle.clj
@@ -1,6 +1,49 @@
 (ns fox-goose-bag-of-corn.puzzle)
 
+;; ----------------------------------------------------------------------------------------------------
+;; Rules
+;; fox <--> goose           can't be together without you
+;; goose <--> corn          can't be together without you
+;; boat == you == one       only you on the boat alone or with one other
+;; ----------------------------------------------------------------------------------------------------
+
 (def start-pos [[[:fox :goose :corn :you] [:boat] []]])
+;; [#{:you :fox :goose :corn} #{:boat} #{}]
+
+(def end-pos [#{} #{:boat} #{:fox :goose :corn :you}])
+
+;; Manually created crossing-plan
+(def crossing-plan
+  [ ;; winning path
+   [#{:you :fox :goose :corn}         #{:boat}                        #{}]
+
+   ;; bring goose over
+   [#{:fox :corn}                     #{:boat :you :goose}            #{}]
+   [#{:fox :corn}                     #{:boat}                        #{:you :goose}]
+   ;; return
+   [#{:fox :corn}                     #{:boat :you}                   #{:goose}]
+   [#{:fox :corn :you}                #{:boat}                        #{:goose}]
+
+   ;; bring fox over
+   [#{:corn}                          #{:boat :you :fox}              #{:goose}]
+   [#{:corn}                          #{:boat}                        #{:goose :you :fox}]
+   ;; bring back the goose
+   [#{:corn}                          #{:boat :you :goose}            #{:fox}]
+   [#{:corn :you :goose}              #{:boat}                        #{:fox}]
+
+   ;; bring over the corn
+   [#{:goose}                         #{:boat :you :corn}             #{:fox}]
+   [#{:goose}                         #{:boat}                        #{:fox :you :corn}]
+   ;; return
+   [#{:goose}                         #{:boat :you}                   #{:fox :corn}]
+   [#{:goose :you}                    #{:boat}                        #{:fox :corn}]
+   
+   ;; bring over the corn
+   [#{}                               #{:boat :you :goose}            #{:fox :corn}]
+   [#{}                               #{:boat}                        #{:fox :goose :corn :you}]
+])
 
 (defn river-crossing-plan []
-  start-pos)
+  crossing-plan)
+
+
